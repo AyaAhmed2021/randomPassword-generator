ca8285a527d7288a8750fde52f912cced3eefef7
AyaAhmed2021 <ayaahmed22793@gmail.com>
2022-12-01 14:38:51 +0200

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'randomPassword-Generator';
+  password;
+  symbol = false;
+
+  checkSymbol() {
+    console.log('symbol');
+    this.symbol =!this.symbol;
+    console.log(this.symbol);
+
+  }
+
+  generatePassword() {
+    if (this.symbol) {
+      this.password = Array(10)
+        .fill(
+          '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
+        )
+        .map(function (x) {
+          return x[Math.floor(Math.random() * x.length)];
+        })
+        .join('');
+      console.log('second');
+
+      console.log(this.password);
+    } else {
+      this.password = Math.random().toString(36).slice(-8);
+      console.log('first');
+      console.log(this.password);
+    }
+  }
+
+  copyPassword(){
+    navigator.clipboard.writeText(this.password).then().catch(e => console.error(e));
+  }
 }
